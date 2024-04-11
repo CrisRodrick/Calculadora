44f8a39e8f1286092c3a5d0a10e29cc951927c2b
CrisRodrick <ctnrodriguez@gmail.com>
2024-04-11 11:12:29 -0400

    Version 1

---
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.calculadora;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Inflar vista
+        setContentView(R.layout.activity_main);
+
+        //recibir datos desde layout
+        EditText text1 = findViewById(R.id.valor1);
+        EditText text2 = findViewById(R.id.valor2);
+        TextView text3 = findViewById(R.id.resultado);
+        Button btn1 = findViewById(R.id.suma);
+        Button btn2 = findViewById(R.id.resta);
+        Button btn3 = findViewById(R.id.multip);
+        Button btn4 = findViewById(R.id.division);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+    }
+}
